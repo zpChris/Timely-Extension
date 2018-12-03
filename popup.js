f2486cafef905a21ff01333ec9e595741b7b01b4
Chris Elliott <elliott77chris@gmail.com>
2018-12-02 22:01:11 -0800

    General Updates
    Improve some button UI, change pie chart to percentages, display data below, and fix bugs with counting software.

--- popup.js
+++ popup.js
@@ -1,16 +1,24 @@
 var seconds; // Integer variable to keep track of seconds
-var intervalId; // The interval ID of the current setInterval() function. Cleared on new tabs
+var intervalIdCount; // The interval ID of the current setInterval() function. Cleared on new tabs
+var intervalIdDisplay;
+var intervalIdChart;
 
 /*
 
-    LISTENERS TO SET OFF OTHER FUNCTIONS
+    LISTENERS OR FUNCTIONS TO RUN APP
 
 */
 
+update_tab_info(); // Always updating time
+
 // When window loads, update app and extension html
 window.onload = function () {
-    update_tab_info();
     update_extension_html();
+    document.getElementById("sp-clear-storage").onclick = function () {
+        chrome.storage.local.clear();
+        seconds = 0;
+        chrome.runtime.reload(); // Forced to restart extension to reset seconds
+    }
 }
 
 // When tab changes, update app
@@ -18,6 +26,7 @@ chrome.tabs.onActivated.addListener(function () {
     // Restart timer loop with new url
     update_tab_info();
 });
+
 // When URL on same tab changes, update app
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     // Since the onUpdated function fires multiple times, update it only on !undefined
@@ -27,7 +36,6 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
         update_tab_info();
     }
 });
-
 
 /*
 
@@ -49,10 +57,10 @@ function update_tab_info() {
             seconds = result[base_url];
         });
 
-        clearInterval(intervalId); // Clear the previous interval before starting anew
+        clearInterval(intervalIdCount); // Clear the previous interval before starting anew
 
         // Set interval ID and start the counter for that site
-        intervalId = setInterval(function () {
+        intervalIdCount = setInterval(function () {
             seconds++;
             chrome.storage.local.set({ [base_url]: seconds });
         }, 1000);
@@ -78,14 +86,30 @@ function update_extension_html() {
 
         pie_chart();
 
+
+        /* All keys/values */
+        chrome.storage.local.get(null, function(items) {
+            var site_stats = sort_object_by_value(items);
+            var allKeys = Object.keys(site_stats);
+            var allValues = Object.values(site_stats);
+            for (i = 0; i < allKeys.length; i++) {
+                var h = Math.floor(allValues[i]/3600);
+                var m = Math.floor((allValues[i]%3600)/60);
+                var s = Math.floor(allValues[i]%60);
+                document.getElementById("sp-sitetrack-storage").innerHTML += "<span id='mydot'></span><b>" + allKeys[i] + ":</b> " + h + "h, " + m + "m, " + s + "s" + "<hr>";
+            }
+        });
+
         // Display url
         document.getElementById("sp-weblink").innerHTML = base_url;
         // Fetch the seconds from chrome.storage, it will constantly update
 
-        setInterval(function () {
+        clearInterval(intervalIdDisplay); // Clear the previous interval before starting anew
+
+        intervalIdDisplay = setInterval(function () {
 
             chrome.storage.local.get({ [base_url]: 0 }, function (result) {
-                document.getElementById("sp-webtimer").innerHTML = result[base_url] + " seconds";
+                document.getElementById("sp-webtimer").innerHTML = result[base_url] + "s";
             });
 
         }, 1000);
@@ -105,18 +129,65 @@ function pie_chart() {
 
     // Print out all keys/values in storage
     chrome.storage.local.get(null, function (items) {
-        
         // Sort objects by value to allow correct input into pie chart
-        var site_stats = sort_object_by_value(items);
+        var site_stats_whole = sort_object_by_value(items); // all sites enumerated
+        var remainder = 0; // remainder for the 'other' category
+        var site_stats = {}; // put site past 5 into 'other'
+        var display_number = 5;
+        for (i = 0; i < Object.keys(site_stats_whole).length; i++) {
+            var key = Object.keys(site_stats_whole)[i];
+            var value = Object.values(site_stats_whole)[i];
+            if (i > display_number-1) {
+                remainder += value;
+            } else {
+                site_stats[key] = value;
+            }
+        }
+        if (remainder != 0) {
+            site_stats["other"] = remainder;
+        }
+        var site_stats_percent = convert_to_percent(site_stats);
 
         var ctx = document.getElementById("myChart").getContext('2d');
         var myChart = new Chart(ctx, {
-            type: 'bar',
+            type: 'pie',
+            options: {
+                response: false,
+                tooltips: {
+                    enabled: true,
+                    mode: 'single',
+                    callbacks: {
+                        title: function (tooltipItem, data) {
+                            return data['labels'][tooltipItem[0]['index']];
+                        },
+                        label: function (tooltipItem, data) {
+                            return data['datasets'][0]['data'][tooltipItem['index']] + '%, ';
+                        }
+                    },
+                    backgroundColor: '#FFF',
+                    titleFontSize: 16,
+                    titleFontColor: '#000',
+                    bodyFontColor: '#000',
+                    bodyFontSize: 14,
+                    displayColors: false
+                },
+                title: {
+                    display: true,
+                    position: 'top',
+                    text: 'Time Tracker'
+                },
+                legend: {
+                    display: false
+                },
+                animation: {
+                    duration: 0,
+                },
+            },
             data: {
-                labels: Object.keys(site_stats),
+                labels: Object.keys(site_stats_percent),
                 datasets: [{
                     label: 'Seconds',
-                    data: Object.values(site_stats),
+                    data: Object.values(site_stats_percent),
                     backgroundColor: [
                         'rgba(255, 99, 132, 0.2)',
                         'rgba(54, 162, 235, 0.2)',
@@ -145,9 +216,6 @@ function pie_chart() {
 // Sort object by value; personalized for the object function by combining remaining elements
 function sort_object_by_value(items) {
 
-    var display_number = 5;
-    var remainder = 0;
-
     var sortable = [];
     for (var item in items) {
         sortable.push([item, items[item]]);
@@ -162,16 +230,22 @@ function sort_object_by_value(items) {
     for (i = 0; i < sortable.length; i++) {
         var key = Object.values(sortable)[i][0];
         var value = Object.values(sortable)[i][1];
-        if (i > display_number-1) {
-            remainder += value;
-        } else {
-            object[key] = value;
-        }
-    }
-    if (remainder != 0) {
-        object["other"] = remainder;
+        object[key] = value;
     }
 
     return object;
 
+}
+
+// Convert to percentages, specialized for input above
+function convert_to_percent(items) {
+    var aggregate = 0;
+    Object.values(items).forEach(function(item) {
+        aggregate += item;
+    });
+    var object = {};
+    Object.keys(items).forEach(function(item) {
+        object[item] = Math.round(items[item]/aggregate * 1000) / 10;
+    });
+    return object;
 }
